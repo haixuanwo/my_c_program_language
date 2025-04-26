aeb610df8ea3636ad092cb6b91af39c0bb26726c
Clark <haixuanwoTxh@gmail.com>
2025-04-27 00:10:58 +0800

    添加游戏状态控制

---
+++ 28_guess_nummber_game/guess_nummber_game.h
@@ -0,0 +1,15 @@
+/*
+ * @Author: Clark
+ * @Email: haixuanwoTxh@gmail.com
+ * @Date: 2025-04-26 00:05:51
+ * @LastEditors: Clark
+ * @LastEditTime: 2025-04-26 00:05:55
+ * @Description: file content
+ */
+
+#ifndef GUESS_NUMMBER_GAME_H
+#define GUESS_NUMMBER_GAME_H
+
+int run_game();
+
+#endif
---
+++ 28_guess_nummber_game/main.c
@@ -0,0 +1,8 @@
+
+#include "guess_nummber_game.h"
+
+int main()
+{
+    run_game();
+    return 0;
+}
